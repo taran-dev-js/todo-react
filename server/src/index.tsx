d7890225ac66136463dda822e39f51c8f42f3541
Roman Taran <roman.taran.dev@gmail.com>
2021-05-12 13:16:17 +0300

    first commit

---
+++ server/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// import App from "./components/App";
+
+const App = () => (<h1>APP</h1>)
+
+ReactDOM.render (
+    <App/>,
+    document.getElementById("root")
+);
